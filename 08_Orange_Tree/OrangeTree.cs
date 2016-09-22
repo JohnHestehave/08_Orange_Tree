b9053ab33c37e61481c9bd7d283b581a0f26cf1a
John <John@JOHN>
2016-09-22 08:41:41 +0200

    Add project files.

---
+++ 08_Orange_Tree/OrangeTree.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Orange_Tree
+{
+	class OrangeTree
+	{
+		public int Age = 0;
+		public int Height = 0;
+		public int NumOranges = 0;
+		public int OrangesEaten = 0;
+		public bool TreeAlive = false;
+
+		public OrangeTree(int startage, int startheight)
+		{
+			Age = startage;
+			Height = startheight;
+			TreeAlive = true;
+		}
+
+		public void OneYearPasses()
+		{
+			Age++;
+			Height += 2;
+			if(Age >= 80)
+			{
+				TreeAlive = false;
+				return;
+			}
+			else if(Age >= 2)
+			{
+				NumOranges += 5;
+			}
+		}
+
+		public void EatOrange(int amount)
+		{
+			if(NumOranges >= amount)
+			{
+				NumOranges -= amount;
+				OrangesEaten += amount;
+			}else
+			{
+				throw new IndexOutOfRangeException("You can't eat an orange that isn't there!  There are 0 oranges available to eat");
+			}
+		} 
+
+	}
+}
